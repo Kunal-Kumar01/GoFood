2bedeb00899c208f2f42ac567db8e97f18d76cba
Kunal Kumar <kunal.lohano@gmail.com>
2024-10-24 23:32:08 +0500

    Make changes to Navbar and Add Router Dom

--- delivery-app/src/App.js
+++ delivery-app/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import Home from './screens/Home';
-
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Login from './screens/Login';
 function App() {
   return (
-    <div ><Home/></div>
+    <Router>
+    
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path = "/Login" element={<Login/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
